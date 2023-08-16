ccc2aa5c96b721a782a9ba453e1cc80e6db27618
= <=>
2023-08-16 22:10:07 +0530

    Bug fixed of program not terminating in EvenOddExample1.java

--- Advanced Java/Multithreading/EvenOddExample1.java
+++ Advanced Java/Multithreading/EvenOddExample1.java
@@ -1,58 +1,72 @@
-class Even implements Runnable{
+class Even implements Runnable {
     private Object lock;
-    public Even(Object lock){
+
+    public Even(Object lock) {
         this.lock = lock;
     }
-    public void printEven(){
-        synchronized(lock){
-            for(int i=0;i<=10;i++){
-                if(i%2==0){
-                    System.out.print(i+" ");
-                    lock.notifyAll();
-                    try{
-                        lock.wait();
-                    }catch(InterruptedException e){
-                        e.printStackTrace();
+
+    public void printEven() {
+        synchronized (lock) {
+            for (int i = 0; i <= 10; i += 2) {
+                System.out.print(i + " ");
+                lock.notify(); // Notify the other thread
+                try {
+                    if (i < 10) {
+                        lock.wait(); // Wait for the other thread to notify
                     }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         }
     }
-    public void run(){
+
+    public void run() {
         printEven();
     }
 }
-class Odd implements Runnable{
+
+class Odd implements Runnable {
     private Object lock;
-    public Odd(Object lock){
+
+    public Odd(Object lock) {
         this.lock = lock;
     }
-    public void printOdd(){
-        synchronized(lock){
-            for(int i=0;i<=10;i++){
-                if(i%2!=0){
-                    System.out.println(i+" ");
-                    lock.notifyAll();
-                     try{
-                        lock.wait();
-                    }catch(InterruptedException e){
-                        e.printStackTrace();
+
+    public void printOdd() {
+        synchronized (lock) {
+            for (int i = 1; i <= 10; i += 2) {
+                System.out.print(i + " ");
+                lock.notify(); // Notify the other thread
+                try {
+                    if (i < 9) {
+                        lock.wait(); // Wait for the other thread to notify
                     }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         }
     }
-    
-    public void run(){
+
+    public void run() {
         printOdd();
     }
 }
-public class EvenOddExample1{
-    public static void  main(String args[]){
+
+public class EvenOddExample1 {
+    public static void main(String args[]) {
         Object lock = new Object();
         Thread even = new Thread(new Even(lock));
         Thread odd = new Thread(new Odd(lock));
         even.start();
         odd.start();
+        try {
+            even.join();
+            odd.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("\nAfter both have executed");
     }
 }
